5abd268f394369c414c27c5734f876c419cf7286
Aris Panagiotopoulos <extern.aristeidis.panagiotopoulos@ckc.de>
2019-08-15 19:48:29 +0300

    set to not draft lab session

--- lib/instructor/components/instructor_forms.dart
+++ lib/instructor/components/instructor_forms.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:funlab/common/models/lab_session.model.dart';
 import 'package:funlab/common/reducers/app_state.dart';
+import 'package:funlab/common/reducers/current_lab_session_Id.dart';
 import 'package:funlab/common/services/lab_session.service.dart';
 import 'package:funlab/common/widgets/custom_toaster.dart';
 import 'package:funlab/common/widgets/edit_button.dart';
@@ -16,42 +17,47 @@ class InstructorForms extends StatelessWidget {
     return StoreConnector<AppState, LabSession>(
         converter: (store) => store.state.currentLabSession,
         builder: (context, labSession) {
-          return labSession == null ? 
-          
-          LabSessionForm() : 
-          
-          Column(children: <Widget>[
-          
-            ActivityForm(),
-
-            EditLabSessionButton(labSession: labSession,)
-          ]);
+          return labSession == null
+              ? LabSessionForm()
+              : Column(children: <Widget>[
+                  ActivityForm(),
+                  EditLabSessionButton(
+                    labSession: labSession,
+                  )
+                ]);
         });
   }
 }
 
 class EditLabSessionButton extends StatelessWidget {
   const EditLabSessionButton({
-    Key key, this.labSession,
+    Key key,
+    this.labSession,
   }) : super(key: key);
 
   final labSession;
-  
+
   @override
   Widget build(BuildContext context) {
-    return EditButton('Publish Lab Session', (){
-
-      LabSession newSession = LabSession(title: labSession.title, id: labSession.id, draft: true);
+    return StoreConnector<AppState, OnStateChanged>(converter: (store) {
+      return (labSession) =>
+          store.dispatch(ReplaceCurrentLabAction(labSession));
+    }, builder: (context, callback) {
+      return EditButton('Publish Lab Session', () {
+        LabSession newSession =
+            LabSession(title: labSession.title, id: labSession.id, draft: false, finished: labSession.finished);
 
         HttpService().putRequest(newSession, newSession.id, (success, id) {
           if (success) {
-            CustomToaster().showToast(context, ToasterType.success,
-                'Lab Session is published');
+            callback(newSession);
+            CustomToaster().showToast(
+                context, ToasterType.success, 'Lab Session is published');
           } else {
-            CustomToaster().showToast(context, ToasterType.failure,
-                'Lab Session publish failed');
+            CustomToaster().showToast(
+                context, ToasterType.failure, 'Lab Session publish failed');
           }
         });
+      });
     });
   }
 }
