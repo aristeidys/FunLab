847182008041784d651d9b8def1c0724cb7a7f3a
Aris Panagiotopoulos <apanagiotopoulos@hello-world.services>
2019-09-28 20:29:35 +0300

    Fix the controllers

---
+++ Backend/app/controllers/sessions_controller.rb
@@ -0,0 +1,56 @@
+class SessionsController < ApplicationController
+  
+  before_action :set_session, only: [:show, :update, :destroy]
+  
+  
+  def index
+
+      # GET /sessions?title=hello
+
+    @sessions = if params[:title]
+      Session.search_by_title(params[:title])
+    else
+        # GET /sessions
+
+      Session.all
+    end
+    @sessions = @sessions.findByTitle(params[:title]) if params[:title].present?
+    
+    render json: @sessions
+  end
+  
+  # GET /sessions/23
+  def show
+    render json: @sessions
+  end
+
+  # POST /sessions
+  def create
+    @sessions = Sessions.new(session_params)
+    if @sessions.save
+      render :show, status: :created, location: @session
+    else
+      render json: @sessions.errors, status: :unprocessable_entity
+    end
+  end
+  
+  def update
+    if @session.update(labSession_params)
+      render :show, status: :ok, location: @session
+    else 
+      render json: @session.errors, status: :unprocessable_entity
+    end
+  end
+  
+  def destroy
+    @session.destroy
+  end
+  
+  private
+      def set_session
+        @session = Session.find(params[:id])
+      end
+      def session_params
+        params.require(:session).permit(:title, :finished, :draft)
+      end
+end
--- Backend/config/routes.rb
+++ Backend/config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :lab_sessions, defaults: {format: :json}  do
-    resources :activities
-    resources :enrolments
+  resources :sessions, defaults: {format: :json}  do
+    resources :tasks
   end
-  resources :activities, defaults: {format: :json}
+
+  resources :tasks, defaults: {format: :json}
   
   resources :students, defaults: {format: :json}  do
-    resources :enrolments
+    resources :task_results
+    resources :session_results
   end
-  resources :enrolments, defaults: {format: :json}
+  resources :, defaults: {format: :json}  do
+    resources :task_results
+  end
+  resources :instructors, defaults: {format: :json}
 end
