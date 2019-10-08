0b97cefcb936b63f5f8358f5845f2f9506f3e268
Aris Panagiotopoulos <arispanagiwtopoulos@gmail.com>
2019-10-08 21:54:38 +0300

    fix

--- Apps/ios/Flutter/flutter_export_environment.sh
+++ Apps/ios/Flutter/flutter_export_environment.sh
@@ -2,7 +2,7 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/apanagio/Library/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/apanagio/Documents/Projects/FunLab/Apps"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib/common/helpers/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/apanagio/Library/flutter/bin/cache/artifacts/engine/ios"
