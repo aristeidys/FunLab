5b56f347f8b88cb6233d888fe7f4e698e2814b1e
Aris Panagiotopoulos <arispanagiwtopoulos@gmail.com>
2019-10-13 19:22:00 +0300

    test classroom model

--- Backend/test/models/classroom_test.rb
+++ Backend/test/models/classroom_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 class ClassroomTest < ActiveSupport::TestCase
 
+  # Instantiation
+
   test "save Classroom without name" do
     classroom = Classroom.new
     classroom.instructor_id = instructors(:sneip).id
@@ -17,6 +19,7 @@ class ClassroomTest < ActiveSupport::TestCase
   end
 
   test "save Classroom without instructor" do
+
     classroom = Classroom.new
     classroom.name = "smalligood"
 
@@ -24,6 +27,7 @@ class ClassroomTest < ActiveSupport::TestCase
   end
 
   test "save Classroom successfully" do
+
     classroom = validClassroom()
     
     assert classroom.save, "Saved the classroom successfully"
@@ -32,16 +36,48 @@ class ClassroomTest < ActiveSupport::TestCase
   test "save Classroom without unique name" do
 
     classroom = validClassroom()
-
     assert classroom.save
-
     classroom2 = validClassroom()
-
 
     assert_not classroom2.save, "Saved the classroom without unique name"
   end
 
+  # Search
+
+  test "find Classroom by name" do
+
+    classroomFound = Classroom.findByName("The best classroom").first
+
+    assert_not_nil classroomFound, "Didn't find classroom by name"
+  end
+
+  test "find Classroom by nonexisting name" do
+
+    classroomFound = Classroom.findByName("not existing name").first
+
+    assert_nil classroomFound, "Found unknown classroom by name"
+  end
+
+  test "find Classroom by instructor_id" do
+
+    validClassroom().save
+
+    classroomFound = Classroom.findByParentID(1).first
+
+    assert_not_nil classroomFound, "Didn't find classroom by instructor_id"
+  end
+
+  test "find Classroom by nonexisting instructor_id" do
+
+    validClassroom().save
+
+    classroomFound = Classroom.findByParentID(14234).first
+
+    assert_nil classroomFound, "Found classroom by invalid instructor_id"
+  end
+
   private
+
   def validClassroom
     classroom = Classroom.new
     classroom.name = "valid Name"
