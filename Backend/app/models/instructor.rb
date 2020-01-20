123b321ab4a201170e33237a4d0d69d31be18d7b
Aris Panagiotopoulos <arispanagiwtopoulos@gmail.com>
2020-01-20 21:11:34 +0200

    update validations

--- Backend/app/models/instructor.rb
+++ Backend/app/models/instructor.rb
@@ -1,15 +1,14 @@
 class Instructor < ApplicationRecord
       
   # Validations
-  # validates :name, :length => { :in => 6..45 }
-  # validates :email, :length => { :in => 6..40 }
-  # validates :password, :length => { :in => 8..40 }
-  # validates :email, uniqueness: true
-  # validates :name, :email, :password, 
-  # validates :email, :length => { :in => 3..40 }
+  validates :name, :length => { :in => 4..45 }
+  validates :email, :length => { :in => 6..40 }
+  validates :password, :length => { :in => 8..40 }
+
+  validates :name, :email, :password, 
   validates :email, uniqueness: true
   validates :email, presence: true
-
+  
   # Search
   scope :findByEmail, -> (email) { where email: email }
 
--- Backend/app/models/student.rb
+++ Backend/app/models/student.rb
@@ -1,13 +1,11 @@
 class Student < ApplicationRecord
       
   # Validations
-  # validates :name, :length => { :in => 6..45 }
-  # validates :email, :length => { :in => 6..40 }
-  # validates :password, :length => { :in => 8..40 }
-  # validates :email, uniqueness: true
-  # validates :name, :email, :password, 
-  # validates :email, :length => { :in => 3..40 }
-  
+  validates :name, :length => { :in => 4..45 }
+  validates :email, :length => { :in => 6..40 }
+  validates :password, :length => { :in => 8..40 }
+
+  validates :name, :email, :password, 
   validates :email, uniqueness: true
   validates :email, presence: true
 
