1ec18151fe7100912ef1c7d976f488d10f362b60
Aris Panagiotopoulos <arispanagiwtopoulos@gmail.com>
2020-01-21 15:53:28 +0200

    Add credentials validations

--- Backend/app/models/instructor.rb
+++ Backend/app/models/instructor.rb
@@ -5,9 +5,10 @@ class Instructor < ApplicationRecord
   validates :email, :length => { :in => 6..40 }
   validates :password, :length => { :in => 8..40 }
 
-  validates :name, :email, :password, 
   validates :email, uniqueness: true
   validates :email, presence: true
+  validates :name, presence: true
+  validates :password, presence: true
   
   # Search
   scope :findByEmail, -> (email) { where email: email }
